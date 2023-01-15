68fe8fea72cb47023375290773dd51975cc2a0bc
makerstoday <marcelobarbosafb@gmail.com>
2023-01-15 13:18:06 -0300

    iniciando a logica
    estruturando elementos/funções/eventos

---
+++ javascript/scripts.js
@@ -0,0 +1,23 @@
+//seleção de elementos//
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+
+
+//funções//
+
+
+
+
+
+
+//eventos//
+todoForm.addEventListener("submit", (e) => {
+    e.preventDeFault();
+
+    console.log("enviou form");
+});
